04369eb3f4010b38de55b56c17f6527153bf37d5
Bashir Egeh <b_egeh@hotmail.com>
2020-01-14 18:00:17 +0000

    linted

--- spinner2.js
+++ spinner2.js
@@ -1,4 +1,4 @@
-let arr = ['|', "/", "-", '\\','|', "/", "-", '\\'];
+let arr = ['|', "/", "-", '\\','|', "/", "-", '\\'];  
 for (let i = 0; i < arr.length; i++) {
   setTimeout(() => {
     process.stdout.write(`\r${arr[i]}   `);
